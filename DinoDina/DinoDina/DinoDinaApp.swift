88c795194b6e49fd41ef088db73e458b8c862588
zhunhe <xuzhunhe@gmail.com>
2023-05-02 14:05:41 +0900

    Initial commit

---
+++ DinoDina/DinoDina/DinoDinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoDinaApp.swift
+//  DinoDina
+//
+//  Created by 허준혁 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct DinoDinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
