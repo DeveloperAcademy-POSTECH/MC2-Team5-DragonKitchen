8e8e34aca5e2b08c325c97c8be5a7baa86454952
Kiyoung <rlaky45@naver.com>
2023-05-03 11:58:48 +0900

    chore: UIscreen width, height extension 추가

---
+++ DinoDina/DinoDina/Util/Extension /Extension+UIscreen.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+UIscreen.swift
+//  DinoDina
+//
+//  Created by 김기영 on 2023/05/03.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen {
+    
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+
+    
+}
