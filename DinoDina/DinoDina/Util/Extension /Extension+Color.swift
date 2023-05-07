8c1660fc5f1f0fae00722a428fc868f0da7e4777
Kiyoung <rlaky45@naver.com>
2023-05-07 21:21:51 +0900

    chore: Color Extension에 vegiGreen, fruitRed, meatYellow 추가

---
+++ DinoDina/DinoDina/Util/Extension /Extension+Color.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+Color.swift
+//  DinoDina
+//
+//  Created by 김기영 on 2023/05/07.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1){
+        self.init(.sRGB, red: Double((hex >> 16) & 0xff) / 255, green: Double((hex >> 08) & 0xff) / 255, blue: Double((hex >> 00) & 0xff) / 255, opacity: alpha
+        )
+    }
+    
+    
+    static let vegiGreen:Color = Color(hex: 0x41AF39)
+    static let fruitRed:Color = Color(hex: 0xE20000)
+    static let meatYellow:Color = Color(hex: 0xFFBA00)
+    
+}
