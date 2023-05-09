c26087933376e97d9c8cc3d361596d7620a22bb2
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-09 19:36:05 +0900

    refactor: ColorQuestView 내 ColorButtonView로 기능 분리
    - 색 버튼 6개 용 view 따로 분리

--- DinoDina/DinoDina/Views/ColorQuestView.swift
+++ DinoDina/DinoDina/Views/ColorQuestView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 
 struct ColorQuestView: View {
 
-    let colors: [Color] = [Color.fruitRed, Color.vegiGreen, Color.meatYellow, .orange, .blue, .brown]
     @State var selectedColor : Int = 0
     @State var isCleared: Bool = false
     @State var paprikaImage: String = "paprikaGray"
@@ -32,44 +31,48 @@ struct ColorQuestView: View {
                 HStack {
                     Image(paprikaImage)
                     
-                    Spacer()
+                    
                     
                     // 색 버튼 6개
-                    LazyVGrid(
-                        columns: [
-                            GridItem(.fixed(100), spacing: 23),
-                            GridItem(.fixed(100), spacing: 23),
-                            GridItem(.fixed(100))
-                        ],
-                        spacing: 13) {
-                            ForEach(colors.indices) { index in
-                                Button(action: {
-                                    isCleared = true
-                                    selectedColor = index
-                                    paprikaImage = "paprika"
-                                }) {
-                                    Circle()
-                                        .fill(colors[index])
-                                        .overlay(Circle().strokeBorder(Color.white, lineWidth: 6))
-                                        .overlay(Circle().strokeBorder(Color.gray, lineWidth: 1))
-                                        .frame(width: 100, height: 100)
-                                }
-                            }
-                        }
+                    ColorButton(isCleared: $isCleared, paprikaImage: $paprikaImage, selectedColor: $selectedColor)
                 } // -------- HStack
             } // --------- VStack
         } // --------- ZStack
-      .frame(width: UIScreen.width, height: UIScreen.height, alignment: .center)
     }
 }
 
-//struct ImageChange {
-//    @Binding var selectedColor : Int
-//
-//    if (selectedColor = 0) {
-//
-//    }
-//}
+struct ColorButton : View {
+    
+    @Binding var isCleared: Bool
+    @Binding var paprikaImage: String
+    @Binding var selectedColor : Int
+    
+    let colors: [Color] = [Color.fruitRed, Color.vegiGreen, Color.meatYellow, .orange, .blue, .brown]
+    
+    var body: some View {
+        LazyVGrid(
+            columns: [
+                GridItem(.fixed(100), spacing: 23),
+                GridItem(.fixed(100), spacing: 23),
+                GridItem(.fixed(100))
+            ],
+            spacing: 13) {
+                ForEach(colors.indices) { index in
+                    Button(action: {
+                        isCleared = true
+                        selectedColor = index
+                        paprikaImage = "paprika"
+                    }) {
+                        Circle()
+                            .fill(colors[index])
+                            .overlay(Circle().strokeBorder(Color.white, lineWidth: 6))
+                            .overlay(Circle().strokeBorder(Color.gray, lineWidth: 1))
+                            .frame(width: 100, height: 100)
+                    }
+                }
+            }
+    }
+}
 
 struct ColorQuestView_Previews: PreviewProvider {
     static var previews: some View {
