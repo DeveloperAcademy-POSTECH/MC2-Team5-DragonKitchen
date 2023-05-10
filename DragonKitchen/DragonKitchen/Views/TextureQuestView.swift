aa69a061cd905407bb221516b1892b817d000242
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-11 01:34:41 +0900

    chore: 파프리카 색상&재질 이미지 asset 생성 및 폴더정리
    - 재질 버튼 이미지 6개 등록
    - 재질 입힌 파프리카 이미지 6개 등록 (빨강)
    - 파프리카 색상 관련 이미지끼리 폴더로 묶음

---
+++ DragonKitchen/DragonKitchen/Views/TextureQuestView.swift
@@ -0,0 +1,20 @@
+//
+//  TextureQuestView.swift
+//  DragonKitchen
+//
+//  Created by Seohyun Hwang on 2023/05/11.
+//
+
+import SwiftUI
+
+struct TextureQuestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextureQuestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextureQuestView()
+    }
+}
