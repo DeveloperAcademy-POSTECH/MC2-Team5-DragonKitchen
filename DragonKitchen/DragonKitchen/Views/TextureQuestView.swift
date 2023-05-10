76d2fe112a29eff282f07bc10fb9ab82fa5bea4f
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-11 01:35:51 +0900

    feat: [퀘스트 - 촉각] TextureQuestView 생성 및 구현중
    - 6개 촉각버튼 생성 후 원 안에 재질 이미지 넣기

--- DragonKitchen/DragonKitchen/Views/TextureQuestView.swift
+++ DragonKitchen/DragonKitchen/Views/TextureQuestView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct TextureQuestView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    let textures: [String] = ["basketballButton", "glassButton", "sandButton", "golfButton", "woodButton", "woolButton"]
+    @State var selectedColor : Int = 0
+    @State var isCleared: Bool = false
+       
+       var body: some View {
+           
+           ZStack {
+               // 테이블 배경
+               Image("tableBG1")
+                   .resizable()
+                   .ignoresSafeArea()
+               
+               VStack {
+                   // 네비게이션바
+                   NavigationBar(isCleared: $isCleared)
+                   
+                   Spacer()
+                   
+                   HStack {
+                       Image(paprikaImage)
+                       
+                       Spacer()
+                       
+                       // 색 버튼 6개
+                       LazyVGrid(
+                           columns: [
+                               GridItem(.fixed(100), spacing: 23),
+                               GridItem(.fixed(100), spacing: 23),
+                               GridItem(.fixed(100))
+                           ],
+                           spacing: 13) {
+                               ForEach(textures.indices, id: \.self) { index in
+                                   Button(action: {
+                                       isCleared = true
+                                       selectedColor = index
+                                       paprikaImage = "paprika"
+                                   }) {
+                                       Image(textures[index])
+                                           .resizable()
+                                           .clipShape(Circle().size(width: 100, height: 100))
+                                           .overlay(Circle().strokeBorder(Color.white, lineWidth: 6))
+                                           .overlay(Circle().strokeBorder(Color.gray, lineWidth: 1))
+                                           .frame(width: 100, height: 100)
+                                   }
+                               }
+                           }
+                   } // -------- HStack
+               } // --------- VStack
+           } // --------- ZStack
+       }
+   }
+
 
 struct TextureQuestView_Previews: PreviewProvider {
     static var previews: some View {
-        TextureQuestView()
+        TextureQuestView().previewInterfaceOrientation(.landscapeRight)
     }
 }
+
