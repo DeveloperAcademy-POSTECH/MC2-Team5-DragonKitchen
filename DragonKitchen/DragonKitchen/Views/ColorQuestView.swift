ace84928007d23d89cd4a33d7b1ecca489803566
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-11 18:38:11 +0900

    feat: [퀘스트 - 시각] 사진으로 판별된 식재료 이미지를 가져와 흑백 일러스트로 이미지 띄우기 기능 구현
    - 아직 파프리카밖에 안됨 (당근 이미지 asset 미완성)
    - chosenFood 변수 사용하여 식재료명 가져옴

--- DragonKitchen/DragonKitchen/Views/ColorQuestView.swift
+++ DragonKitchen/DragonKitchen/Views/ColorQuestView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 struct ColorQuestView: View {
 
     @State var isCleared: Bool = false
-    @State var foodImage: String = "paprikaGray_shadow"
-    @EnvironmentObject var chosen: ChosenFood
+    @EnvironmentObject var name: ChosenFood
+    @State var foodImage: String = ""
     
     var body: some View {
         
@@ -43,9 +43,11 @@ struct ColorQuestView: View {
                 }
             }
         }
-//        .onAppear{
-//            paprikaImage = chosen.chosenFood
-        
+        .onAppear{
+            if foodImage == "" {
+                foodImage = name.chosenFood + "Gray_shadow"
+            }
+        }
     }
 }
 
@@ -54,10 +56,11 @@ struct ColorButton : View {
     @Binding var isCleared: Bool
     @Binding var foodImage: String
     @State var selectedButton : Int?
+    @EnvironmentObject var color: ChosenFood // 선택한 색상을 저장해두는 변수
     
     let colors: [Color] = [Color.paprikaRed, Color.vegiGreen, Color.paprikaBurgundy, Color.paprikaOrange, Color.paprikaYellow, Color.paprikaBrown]
     
-    let paprikaColor : [String] = ["paprikaRed_shadow", "paprikaGreen_shadow", "paprikaBurgundy_shadow", "paprikaOrange_shadow", "paprikaYellow_shadow", "paprikaBrown_shadow"]
+    let foodColor : [String] = ["paprikaRed_shadow", "paprikaGreen_shadow", "paprikaBurgundy_shadow", "paprikaOrange_shadow", "paprikaYellow_shadow", "paprikaBrown_shadow"]
     
     var body: some View {
         LazyVGrid(
@@ -70,8 +73,28 @@ struct ColorButton : View {
                 ForEach(colors.indices, id: \.self) { index in
                     Button(action: {
                         isCleared = true
-                        foodImage = paprikaColor[index]
+                        foodImage = foodColor[index]
                         selectedButton = index
+                        
+                        if(index == 0) { // 첫번째 버튼 : 빨강
+                            color.chosenColor = "red"
+                        }
+                        else if(index == 1) { // 두번째 버튼 : 초록
+                            color.chosenColor = "green"
+                        }
+                        else if(index == 2) { // 세번째 버튼 : 버건디
+                            color.chosenColor = "burgundy"
+                        }
+                        else if(index == 3) { // 네번째 버튼 : 주황
+                            color.chosenColor = "orange"
+                        }
+                        else if(index == 4) { // 다섯번째 버튼 : 노랑
+                            color.chosenColor = "yellow"
+                        }
+                        else { // 여섯번째 버튼 : 갈색
+                            color.chosenColor = "brown"
+                        }
+                        
                     }) {
                         // 색상 버튼 모양
                         Circle()
@@ -88,7 +111,6 @@ struct ColorButton : View {
 }
 
 struct ColorQuestView_Previews: PreviewProvider {
-//    @Binding var paprikaImage: String
     
     static var previews: some View {
         ColorQuestView().previewInterfaceOrientation(.landscapeRight)
