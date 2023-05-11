a5e3f597155ebe8d2ec40f0ca81c2abf39e075de
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-11 10:59:01 +0900

    feat: [퀘스트 - 촉각] TextureButton() 으로 버튼 조작 기능 분리 / 버튼 기능 구현
    - 버튼에 재질 이미지 넣기
    - 버튼 누를 시 파프리카 이미지 변경 (이미지 보정 해서 asset 추가 예정)

--- DragonKitchen/DragonKitchen/Views/TextureQuestView.swift
+++ DragonKitchen/DragonKitchen/Views/TextureQuestView.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct TextureQuestView: View {
-    let textures: [String] = ["basketballButton", "glassButton", "sandButton", "golfButton", "woodButton", "woolButton"]
+    let textures: [String] = ["basketball", "glass", "sand", "golf", "wood", "wool"]
     @State var selectedColor : Int = 0
     @State var isCleared: Bool = false
-    @State var paprikaImage: String = "paprikaGray_shadow"
-       
+    @EnvironmentObject var chosen: ChosenFood
        var body: some View {
            
            ZStack {
@@ -28,45 +27,68 @@ struct TextureQuestView: View {
                    Spacer()
                    
                    HStack {
-                       Image(paprikaImage)
+                       Image(chosen.chosenFood)
                            .resizable()
                            .scaledToFit()
+                           .frame(width: 250)
+                           .padding(.leading, UIScreen.width/30)
+                           .padding(.bottom, UIScreen.height/30)
                        
                        Spacer()
-                       
-                       // 색 버튼 6개
-                       LazyVGrid(
-                           columns: [
-                               GridItem(.fixed(100), spacing: 23),
-                               GridItem(.fixed(100), spacing: 23),
-                               GridItem(.fixed(100))
-                           ],
-                           spacing: 13) {
-                               ForEach(textures.indices, id: \.self) { index in
-                                   Button(action: {
-                                       isCleared = true
-                                       selectedColor = index
-                                       paprikaImage = "paprika"
-                                   }) {
-                                       Image(textures[index])
-                                           .resizable()
-                                           .clipShape(Circle().size(width: 100, height: 100))
-                                           .overlay(Circle().strokeBorder(Color.white, lineWidth: 6))
-                                           .overlay(Circle().strokeBorder(Color.gray, lineWidth: 1))
-                                           .frame(width: 100, height: 100)
-                                   }
-                               }
-                           }
+                           
+                       // 색상 버튼 6개
+                       TextureButton(isCleared: $isCleared)
                    } // -------- HStack
                } // --------- VStack
            } // --------- ZStack
        }
-   }
+
+struct TextureButton : View {
+    
+    let textures: [String] = ["basketball", "glass", "sand", "golf", "wood", "wool"]
+    
+    @Binding var isCleared: Bool
+    @State var selectedButton : Int?
+    @EnvironmentObject var chosen: ChosenFood
+//    let buttons = [0, 1, 2, 3, 4, 5]
+    
+    let paprikaTexture : [String] = ["sand_red", "sand_green", "sand_burgundy", "sand_orange", "sand_yellow", "sand_brown"]
+    
+    var body: some View {
+        LazyVGrid(
+            columns : [
+                GridItem(.fixed(100), spacing: 23),
+                GridItem(.fixed(100), spacing: 23),
+                GridItem(.fixed(100))
+            ],
+            spacing: 13) {
+                ForEach(textures.indices, id: \.self) { index in
+                    Button(action: {
+                        isCleared = true
+                        chosen.chosenFood = paprikaTexture[index]
+                        selectedButton = index
+                    }) {
+                        // 색상 버튼 모양
+                        Image(textures[index])
+                            .resizable()
+                            .clipShape(Circle())
+                            .overlay(Circle().strokeBorder(Color.white, lineWidth: 6))
+                            .overlay(Circle().stroke(selectedButton == index ? Color.black : Color.gray, lineWidth: selectedButton == index ? 3 : 1))
+                            .frame(width: 100, height: 100)
+                    }
+                }
+            } // 색상 버튼 위치 조정
+            .padding(.trailing, UIScreen.width/15)
+            .padding(.bottom, UIScreen.height/30)
+    }
+}
+}
 
 
 struct TextureQuestView_Previews: PreviewProvider {
     static var previews: some View {
         TextureQuestView().previewInterfaceOrientation(.landscapeRight)
+            .environmentObject(ChosenFood())
     }
 }
 
