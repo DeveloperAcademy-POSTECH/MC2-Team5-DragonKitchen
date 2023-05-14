0635dba73754570020f70af5174fb18e38f5e223
Seohyun Hwang <hw.seohyun06@gmail.com>
2023-05-14 19:19:14 +0900

    feat: FoodColorModel 당근 pattern 색상 hex code 수정

--- DragonKitchen/DragonKitchen/Models/FoodColorModel.swift
+++ DragonKitchen/DragonKitchen/Models/FoodColorModel.swift
@@ -16,10 +16,10 @@ class VegetableColor: ObservableObject {
 class CarrotColor {
     var gray = (Color(hex: 0x7C7C7C), Color(hex: 0x646464), Color(hex: 0x8E8E8E), Color(hex: 0x000000))
     var red = (Color(hex: 0x00AE26), Color(hex: 0xAE0000), Color(hex: 0xE50000), Color(hex: 0x000000))
-    var brown = (Color(hex: 0x00AE26), Color(hex: 0xC9751C), Color(hex: 0x914517), Color(hex: 0x000000))
-    var yellow = (Color(hex: 0x00AE26), Color(hex: 0xC9751C), Color(hex: 0xF6DA00), Color(hex: 0x000000))
-    var green = (Color(hex: 0x00AE26), Color(hex: 0xC9751C), Color(hex: 0x41AF39), Color(hex: 0x000000))
-    var burgundy = (Color(hex: 0x00AE26), Color(hex: 0xC9751C), Color(hex: 0x960A00), Color(hex: 0x000000))
+    var brown = (Color(hex: 0x00AE26), Color(hex: 0x54260B), Color(hex: 0x914517), Color(hex: 0x000000))
+    var yellow = (Color(hex: 0x00AE26), Color(hex: 0xCAB302), Color(hex: 0xF6DA00), Color(hex: 0x000000))
+    var green = (Color(hex: 0x00AE26), Color(hex: 0x187511), Color(hex: 0x41AF39), Color(hex: 0x000000))
+    var burgundy = (Color(hex: 0x00AE26), Color(hex: 0xDA625A), Color(hex: 0x960A00), Color(hex: 0x000000))
     var orange = (Color(hex: 0x00AE26), Color(hex: 0xC9751C), Color(hex: 0xFF6F00), Color(hex: 0x000000))
 }
 
