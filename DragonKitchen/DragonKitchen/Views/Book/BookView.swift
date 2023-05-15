fee957bb12953fc5d7df3ae1c3398b158692f9a2
Junhyuk Hur <xuzhunhe@gmail.com>
2023-05-15 11:17:17 +0900

    feat: [도감] 타이틀바 백그라운드 색상 추가

--- DragonKitchen/DragonKitchen/Views/Book/BookView.swift
+++ DragonKitchen/DragonKitchen/Views/Book/BookView.swift
@@ -26,16 +26,20 @@ struct BookView: View {
     ]
     
     var body: some View {
-//        NavigationStackView(transitionType: .none) {
-            ZStack {
-                VStack {
+        ZStack {
+            VStack {
+                ZStack {
+                    ingredientModel.color
+                        .ignoresSafeArea()
+                        .opacity(0.5)
+
                     HStack {
                         Text("나의 먹이 도감")
                             .font(.cookierun(.bold, size: 45))
                             .foregroundColor(.black)
-                        
+
                         Spacer()
-                        
+
                         PushView(destination: MainView()) {
                             Image("BackButton")
                                 .resizable()
@@ -44,92 +48,94 @@ struct BookView: View {
                         }
                     }
                     .padding()
+                }
+                .frame(height: UIScreen.height * 0.2)
 
-                    HStack {
-                        Spacer()
+                HStack {
+                    Spacer()
 
-                        ScrollView(showsIndicators: false, content: {
-                            VStack(alignment: .center) {
-                                LazyVGrid(columns: columns, alignment: .leading) {
-                                    ForEach(ingredientModel.data, id: \.self) { ingredient in
-                                        ZStack {
-                                            IngredientView(ingredientData: ingredient, ingredient: ingredientModel)
-                                                .frame(height: UIScreen.height * 0.37)
-                                                .padding(5)
-                                        }
-                                        .onTapGesture {
-                                            if ingredient.name == chosenFood.chosenFood.1 {
-                                                if let picture = chosenFood.pictureWithIngredient {
-                                                    selectedVegiName = ingredient.name
-                                                    selectedPicture = picture
-                                                    withAnimation(.easeInOut(duration: 0.3)) {
-                                                        isClicked = true
-                                                        isPresenting = true
-                                                    }
+                    ScrollView(showsIndicators: false, content: {
+                        VStack(alignment: .center) {
+                            LazyVGrid(columns: columns, alignment: .leading) {
+                                ForEach(ingredientModel.data, id: \.self) { ingredient in
+                                    ZStack {
+                                        IngredientView(ingredientData: ingredient, ingredient: ingredientModel)
+                                            .frame(height: UIScreen.height * 0.37)
+                                            .padding(5)
+                                    }
+                                    .onTapGesture {
+                                        if ingredient.name == chosenFood.chosenFood.1 {
+                                            if let picture = chosenFood.pictureWithIngredient {
+                                                selectedVegiName = ingredient.name
+                                                selectedPicture = picture
+                                                withAnimation(.easeInOut(duration: 0.3)) {
+                                                    isClicked = true
+                                                    isPresenting = true
                                                 }
                                             }
                                         }
                                     }
                                 }
                             }
-                        })
-                        .frame(width: 500)
+                        }
+                    })
+                    .frame(width: 500)
 
-                        Spacer()
+                    Spacer()
 
-                        VStack {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.vegiGreen)
-                                    .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.black, lineWidth: 2)
-                                    )
-                                Text(Ingredient.vegetable.title)
-                                    .font(.cookierun(.regular))
-                                    .foregroundColor(.black)
-                            }
-                            .onTapGesture {
-                                ingredientModel = .vegetable
-                            }
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.fruitRed)
-                                    .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.black, lineWidth: 2)
-                                    )
-                                Text(Ingredient.fruit.title)
-                                    .font(.cookierun(.regular))
-                                    .foregroundColor(.black)
-                            }
-                            .onTapGesture {
-                                ingredientModel = .fruit
-                            }
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.meatYellow)
-                                    .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.black, lineWidth: 2)
-                                    )
-                                Text(Ingredient.meat.title)
-                                    .font(.cookierun(.regular))
-                                    .foregroundColor(.black)
-                            }
-                            .onTapGesture {
-                                ingredientModel = .meat
-                            }
+                    VStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.vegiGreen)
+                                .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 2)
+                                )
+                            Text(Ingredient.vegetable.title)
+                                .font(.cookierun(.regular))
+                                .foregroundColor(.black)
+                        }
+                        .onTapGesture {
+                            ingredientModel = .vegetable
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.fruitRed)
+                                .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 2)
+                                )
+                            Text(Ingredient.fruit.title)
+                                .font(.cookierun(.regular))
+                                .foregroundColor(.black)
+                        }
+                        .onTapGesture {
+                            ingredientModel = .fruit
+                        }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.meatYellow)
+                                .frame(width: UIScreen.width * 0.15, height: UIScreen.height * 0.1)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 2)
+                                )
+                            Text(Ingredient.meat.title)
+                                .font(.cookierun(.regular))
+                                .foregroundColor(.black)
+                        }
+                        .onTapGesture {
+                            ingredientModel = .meat
                         }
                     }
                 }
-                if isClicked {
-                    Color.black.opacity(0.2).ignoresSafeArea()
-                }
-            }.background(.white)
+            }
+            if isClicked {
+                Color.black.opacity(0.2).ignoresSafeArea()
+            }
+        }.background(.white)
             .overlay {
                 BookPopUpView(
                     isPresenting: $isPresenting,
@@ -139,7 +145,6 @@ struct BookView: View {
                     selectedPicture: $selectedPicture
                 )
             }
-            
-        }
-//    }
+
+    }
 }
